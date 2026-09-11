0855920082c8b00cf193260a731db6ab73d52c23
sujalspecta <sujalspectaclicks@outlook.com>
2026-09-11 11:40:57 +0530

    react18 package install

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SimpleReactLightbox from 'simple-react-lightbox';
+import { createRoot } from "react-dom/client";
 import App from './App';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <SimpleReactLightbox>
-            <App />
-        </SimpleReactLightbox>
-    </React.StrictMode>,
-    document.getElementById('root')
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <SimpleReactLightbox>
+//             <App />
+//         </SimpleReactLightbox>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    // <React.StrictMode>
+        <App />
+    // </React.StrictMode>
 );
-
-reportWebVitals();
